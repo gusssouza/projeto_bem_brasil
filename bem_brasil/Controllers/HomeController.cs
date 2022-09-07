7be7d41f4ce3ed827489be20342556661a24dad2
HyperV <HyperV@JLE106D77.alunos.sc.senac.br>
2022-09-06 22:14:52 -0300

    primeiro commit

---
+++ bem_brasil/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using bem_brasil.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace bem_brasil.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Precisodisso()
+        {
+            return View();
+        }
+
+        public IActionResult Querodoar()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Entrega()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
