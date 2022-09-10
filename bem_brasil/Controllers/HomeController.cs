1b2c60dbb4cbd0b04e34f131b977d741624c0c92
gusssouza <gusssouza23@gmail.com>
2022-09-09 21:49:49 -0300

    tela sobre

--- bem_brasil/Controllers/HomeController.cs
+++ bem_brasil/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace bem_brasil.Controllers
             return View();
         }
 
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ bem_brasil/Views/Home/pedido.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace bem_brasil.Views.Home
+{
+    public class pedidoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
