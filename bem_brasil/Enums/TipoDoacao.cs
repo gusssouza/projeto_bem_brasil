635f5f0a5bfaae592075ef8d73e882ec8a6ff4df
gusssouza <gusssouza23@gmail.com>
2022-09-20 20:39:15 -0300

    region comida, roupas

--- bem_brasil/Enums/TipoDoacao.cs
+++ bem_brasil/Enums/TipoDoacao.cs
@@ -2,5 +2,22 @@
 {
     public enum TipoDoacao
     {
+        #region Comida
+
+        Arroz = 01,
+        Feijao = 02,
+        Carne = 03,
+        CestaBasica = 04,
+
+        #endregion
+
+        #region Roupa
+
+        Inverno = 11,
+        Verao = 12,
+        Infantil = 13,
+        Adulta = 14,
+
+        #endregion
     }
 }
