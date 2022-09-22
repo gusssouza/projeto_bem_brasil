957db9dfa7d6d45ddb3469612ddd0d455c0daa14
Gustavo da Silva de Souza <gusssouza23@gmail.com>
2022-09-22 17:25:20 -0300

    tela doacao e preciso com inputs de endereco

---
+++ bem_brasil/Controllers/ItensDoacaoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bem_brasil.Controllers
+{
+    public class ItensDoacaoController : Controller
+    {
+        private readonly ILogger<ItensDoacaoController> _logger;
+
+        public ItensDoacaoController(ILogger<ItensDoacaoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult DoacaoComida()
+        {
+            return View();
+        }
+
+        public IActionResult DoacaoRoupa()
+        {
+            return View();
+        }
+
+        public IActionResult DoacaoBrinquedo()
+        {
+            return View();
+        }
+    }
+}
