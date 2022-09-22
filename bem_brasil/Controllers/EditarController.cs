d47dd667e7bb9c2c501209cf04eca6a16fc9e5f3
gusssouza <gusssouza23@gmail.com>
2022-09-21 22:18:18 -0300

    telas de doacao e pedidos criadas

---
+++ bem_brasil/Controllers/EditarController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bem_brasil.Controllers
+{
+    public class EditarController : Controller
+    {
+        private readonly ILogger<EditarController> _logger;
+
+        public EditarController(ILogger<EditarController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
