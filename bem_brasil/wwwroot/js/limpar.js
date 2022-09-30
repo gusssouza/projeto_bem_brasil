c55dc67e3b30b1f17bee3f3fbc982280b260feb2
gusssouza <gusssouza23@gmail.com>
2022-09-30 19:28:52 -0300

    limpar inputs

---
+++ bem_brasil/wwwroot/js/limpar.js
@@ -0,0 +1,147 @@
+﻿function doarComida() {
+     let titulo = document.getElementById("tituloDoarComida");
+     let quant = document.getElementById("quantidadeDoarComida");
+     let validade = document.getElementById("validadeDoarComida");
+     let link = document.getElementById("linkDoarComida");
+     let cep = document.getElementById("cepDoarComida");
+     let uf = document.getElementById("ufDoarComida");
+     let cidade = document.getElementById("cidadeDoarComida");
+     let bairro = document.getElementById("bairroDoarComida");
+     let rua = document.getElementById("ruaDoarComida");
+     let num = document.getElementById("numeroCasaDoarComida");
+     let desc = document.getElementById("exampleFormControlTextarea2");
+     validade.value = "";
+     titulo.value = "";
+     quant.value = "";
+     link.value = "";
+     cep.value = "";
+     uf.value = "";
+     cidade.value = "";
+     bairro.value = "";
+     rua.value = "";
+     num.value = "";
+     desc.value = "";
+     alert('Postagem de doação realizada com sucesso');
+}
+
+function doarBrinquedo() {
+    let titulo = document.getElementById("tituloDoarBrinquedo");
+    let quant = document.getElementById("quantDoarBrinquedo");
+    let link = document.getElementById("linkDoarBrinquedo");
+    let cep = document.getElementById("cepDoarBrinquedo");
+    let uf = document.getElementById("ufDoarBrinquedo");
+    let cidade = document.getElementById("cidadeDoarBrinquedo");
+    let bairro = document.getElementById("bairroDoarBrinquedo");
+    let rua = document.getElementById("ruaDoarBrinquedo");
+    let num = document.getElementById("numeroCasaDoarBrinquedo");
+    let desc = document.getElementById("exampleFormControlTextarea2");
+    titulo.value = "";
+    quant.value = "";
+    link.value = "";
+    cep.value = "";
+    uf.value = "";
+    cidade.value = "";
+    bairro.value = "";
+    rua.value = "";
+    num.value = "";
+    desc.value = "";
+    alert('Postagem de doação realizada com sucesso')
+}
+
+function doarRoupa() {
+    let titulo = document.getElementById("tituloDoarRoupa");
+    let quant = document.getElementById("quantidadeDoarRoupa");
+    let link = document.getElementById("linkDoarRoupa");
+    let cep = document.getElementById("cepDoarRoupa");
+    let uf = document.getElementById("ufDoarRoupa");
+    let cidade = document.getElementById("cidadeDoarRoupa");
+    let bairro = document.getElementById("bairroDoarRoupa");
+    let rua = document.getElementById("ruaDoarRoupa");
+    let num = document.getElementById("numeroCasaDoarRoupa");
+    let desc = document.getElementById("exampleFormControlTextarea2");
+    titulo.value = "";
+    quant.value = "";
+    link.value = "";
+    cep.value = "";
+    uf.value = "";
+    cidade.value = "";
+    bairro.value = "";
+    rua.value = "";
+    num.value = "";
+    desc.value = "";
+    alert('Postagem de pedido realizada com sucesso');
+}
+
+function pedirComida() {
+    let titulo = document.getElementById("tituloPedirComida");
+    let quant = document.getElementById("quantidadePedirComida");
+    let validade = document.getElementById("validadePedirComida");
+    let link = document.getElementById("linkPedirComida");
+    let cep = document.getElementById("cepPedirComida");
+    let uf = document.getElementById("ufPedirComida");
+    let cidade = document.getElementById("cidadePedirComida");
+    let bairro = document.getElementById("bairroPedirComida");
+    let rua = document.getElementById("ruaPedirComida");
+    let num = document.getElementById("numeroCasaPedirComida");
+    let desc = document.getElementById("exampleFormControlTextarea2");
+    validade.value = "";
+    titulo.value = "";
+    quant.value = "";
+    link.value = "";
+    cep.value = "";
+    uf.value = "";
+    cidade.value = "";
+    bairro.value = "";
+    rua.value = "";
+    num.value = "";
+    desc.value = "";
+    alert('Postagem de pedido realizada com sucesso');
+}
+
+function pedirRoupa() {
+    let titulo = document.getElementById("tituloPedirRoupa");
+    let quant = document.getElementById("quantPedirRoupa");
+    let link = document.getElementById("linkPedirRoupa");
+    let cep = document.getElementById("cepPedirRoupa");
+    let uf = document.getElementById("ufPedirRoupa");
+    let cidade = document.getElementById("cidadePedirRoupa");
+    let bairro = document.getElementById("bairroPedirRoupa");
+    let rua = document.getElementById("ruaPedirRoupa");
+    let num = document.getElementById("numeroCasaPedirRoupa");
+    let desc = document.getElementById("exampleFormControlTextarea2");
+    titulo.value = "";
+    quant.value = "";
+    link.value = "";
+    cep.value = "";
+    uf.value = "";
+    cidade.value = "";
+    bairro.value = "";
+    rua.value = "";
+    num.value = "";
+    desc.value = "";
+    alert('Postagem de pedido realizada com sucesso');
+}
+
+function pedirBrinquedo() {
+    let titulo = document.getElementById("tituloPedirBrinquedo");
+    let quant = document.getElementById("quantPedirBrinquedo");
+    let link = document.getElementById("linkPedirBrinquedo");
+    let cep = document.getElementById("cepPedirBrinquedo");
+    let uf = document.getElementById("ufPedirBrinquedo");
+    let cidade = document.getElementById("cidadePedirBrinquedo");
+    let bairro = document.getElementById("bairroPedirBrinquedo");
+    let rua = document.getElementById("ruaPedirBrinquedo");
+    let num = document.getElementById("numeroCasaPedirBrinquedo");
+    let desc = document.getElementById("exampleFormControlTextarea2");
+    titulo.value = "";
+    quant.value = "";
+    link.value = "";
+    cep.value = "";
+    uf.value = "";
+    cidade.value = "";
+    bairro.value = "";
+    rua.value = "";
+    num.value = "";
+    desc.value = "";
+    alert('Postagem de pedido realizada com sucesso');
+}
--- bem_brasil/wwwroot/js/site.js
+++ bem_brasil/wwwroot/js/site.js
@@ -1,11 +1,1 @@
-﻿const imagem = (id) => {
-    let imagens = ["../../img/bk2.png", "../../img/bk3.png", "../../img/bk4.png", "../../img/bk5.png", "../../img/bk6.png", "../../img/bk8.png"];
-    let doc = document.getElementById(id);
-    let img = imagens[Math.floor(Math.random() * imagens.length)];
-
-    doc.src = img;
-}
-
-imagem("backgroundPedido");
-imagem("backgroundDoar");
-
+﻿
