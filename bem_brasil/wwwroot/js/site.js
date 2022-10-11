dc1419c12c8af48c79ff4da601b76a6e375b05ad
Nicolas do Nascimento Carvalho <nycocarvalho@outlook.com>
2022-10-11 16:14:01 -0300

    Atualizada tela de anuncio

--- bem_brasil/wwwroot/js/site.js
+++ bem_brasil/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿//mudar a cor e imagem do botão da tela do anúncio;
+function changeImage() {
+    var back = document.getElementById('backColor');
+    var image = document.getElementById('myImage');
+    if ((image.src.match("/img/heart-icon-dark.png")) && (back.style.backgroundColor = "#FFF")) {
+        image.src = "/img/heart-icon-light.png";
+        back.style.backgroundColor = "#3bca78";
+    }
+    else {
+        image.src = "/img/heart-icon-dark.png";
+        back.style.backgroundColor = "#FFF";
+    }
+}
+
