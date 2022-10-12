ea2c48d51434c5ed5f8fc2d96269970278c0742c
Gabriel Retzlaff <gretzlaff13@gmail.com>
2022-10-11 21:07:27 -0300

    atualização cep e login

--- bem_brasil/wwwroot/js/localStorage.js
+++ bem_brasil/wwwroot/js/localStorage.js
@@ -1,8 +1,25 @@
 ﻿localStorage.clear();
 
+document.getElementById("navbarDropdown").style.visibility = "hidden";
+document.getElementById("botaoCadastro").style.visibility = "visible";
+document.getElementById("botaoLogin").style.visibility = "visible";
+
+var logon = false;
+
 const confirmar = () => {
     if (confirm("\nDeseja salvar seus dados de acesso?\n")) {
         save_storage();
+        logon = true;
+    }
+
+    if (logon) {
+        document.getElementById("botaoCadastro").style.visibility = "hidden";
+        document.getElementById("botaoLogin").style.visibility = "hidden";
+        document.getElementById("navbarDropdown").style.visibility = "visible";
+    } else {
+        document.getElementById("navbarDropdown").style.visibility = "hidden";
+        document.getElementById("botaoCadastro").style.visibility = "visible";
+        document.getElementById("botaoLogin").style.visibility = "visible";
     }
     //login
 }
